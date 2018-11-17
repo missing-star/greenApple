27efbb650d2a5416c32521b9c9d27551405e70a0
missing-star <1652575963@qq.com>
2018-11-17 20:40:46 +0800

    修改sha1加密和提交订单页面

--- js/sha1.js
+++ js/sha1.js
@@ -70,8 +70,28 @@ function SHA1(sIn) {
     }  
     SHA1Value = SHA1hex(a) + SHA1hex(b) + SHA1hex(c) + SHA1hex(d) + SHA1hex(e);  
     return SHA1Value.toUpperCase();  
-}  
+}
+function utf16to8(str)	{
+    console.log(str);
+    var out, i, len, c;
 
+    out = "";
+    len = str.length;
+    for(i = 0; i < len; i++){
+        c = str.charCodeAt(i);
+        if ((c >= 0x0001) && (c <= 0x007F)) {
+            out += str.charAt(i);
+        } else if (c > 0x07FF){
+            out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+            out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
+            out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+        } else {
+            out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
+            out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+        }
+    }
+    return out;
+}
 function SHA2(sIn) {  
-    return SHA1(sIn).toLowerCase();  
+    return SHA1(utf16to8(sIn)).toLowerCase();
 }  
